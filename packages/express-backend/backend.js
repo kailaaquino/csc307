342e2afd66ba6908031dad81241620bae090b0ec
Kaila Aquino <kailad.aquino@gmail.com>
2024-04-09 17:20:58 -0700

    ex 2 post

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,17 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
