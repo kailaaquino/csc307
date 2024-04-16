e173eee89ff5c2b6291e6539c2292a36d69ebfba
Kaila Aquino <kailad.aquino@gmail.com>
2024-04-16 16:25:27 -0700

    fetch users from backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"; // 
 import Table from "./Table";
 import Form from "./Form";
 
@@ -23,6 +23,21 @@ import Form from "./Form";
                 //   job: "Bartender"
                 // }
     // ]);
+
+    /* load users from the backend using GET */
+    function fetchUsers() {
+      /* fetch uses http get to get info from the users at that URL
+      promise: makes a promise to return from fetch when info is finally recieved
+      promises return immediately but the info is not always available */
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+  }
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json()) // when the promise if fufilled 
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
           return i !== index;
@@ -32,7 +47,7 @@ import Form from "./Form";
 
       function updateList(person) {
         setCharacters([...characters, person]);
-      }
+      } 
 
       return (
         <div className="container">
