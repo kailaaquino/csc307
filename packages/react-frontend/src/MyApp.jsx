2d21949994cb94be5e499f3e40aac7396da60bc6
Kaila Aquino <kailad.aquino@gmail.com>
2024-04-18 11:59:09 -0700

    201 created

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -24,6 +24,8 @@ import Form from "./Form";
                 // }
     // ]);
 
+    /* HTML methods */
+
     /* load users from the backend using GET */
     function fetchUsers() {
       /* fetch uses http get to get info from the users at that URL
@@ -31,13 +33,28 @@ import Form from "./Form";
       promises return immediately but the info is not always available */
       const promise = fetch("http://localhost:8000/users");
       return promise;
-  }
+    } 
     useEffect(() => {
       fetchUsers()
         .then((res) => res.json()) // when the promise if fufilled 
         .then((json) => setCharacters(json["users_list"]))
         .catch((error) => { console.log(error); });
     }, [] );
+
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+  
+      return promise;
+    }
+
+
+    /* Visual Stuff */ 
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
           return i !== index;
@@ -45,9 +62,30 @@ import Form from "./Form";
         setCharacters(updated);
       }
 
-      function updateList(person) {
-        setCharacters([...characters, person]);
-      } 
+    //   function updateList(person) { 
+    //     postUser(person)
+    //       .then(() => setCharacters([...characters, person]))
+    //       .catch((error) => {
+    //         console.log(error);
+    //       })
+    // } 
+      function updateList(person) { 
+        postUser(person)
+          .then((res) => {
+            /* check for content created */
+            if (res.status === 201){
+              setCharacters([...characters, person]);
+           }
+           /* if the result returns other than 201 */
+            else {
+             console.log('Failed to add user. Status:' + res.status);
+           }  
+         })
+         /* handler for unexpected errors */
+          .catch((error) => {
+            console.log(error);
+          })
+    } 
 
       return (
         <div className="container">
